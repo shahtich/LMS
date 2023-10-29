b51493812d88782767a5ccdfdfec2737394abb99
shahtich <148983238+shahtich@users.noreply.github.com>
2023-10-29 18:39:37 +0500

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,25 @@
+'''
+a= int(input())
+if a%2==0:
+    print('Данное число четное')
+else: 
+    print('Данное число не является четным')
+    '''
+'''
+a= int(input())
+b= int(input())
+if a>b:
+    print(a)
+elif a==b:
+    print('числа равны')
+else:
+    print(b)
+    '''
+'''
+a= int(input())
+b= int(input())
+if (a+b)%2==0:
+    print('black')
+else:
+    print('white')
+    '''
