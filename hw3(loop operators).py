7290a4f812781627780b20445099fb7b1baff96a
shahtich <148983238+shahtich@users.noreply.github.com>
2023-11-01 13:44:33 +0500

    Add files via upload

---
+++ hw3(loop operators).py
@@ -0,0 +1,24 @@
+'''
+n=int(input())
+sum=0
+while n>0:
+    a=n%10
+    sum+=a
+    n=n//10
+print(sum)
+'''
+'''
+n=int(input())
+fac=1
+for i in range(1, n+1, 1):
+    fac=fac*i
+print(fac)
+'''
+"""
+n=int(input())
+for i in range(1,n+1):
+    if i%7==0:
+        print(i)
+else:
+    print('')
+       """ 
