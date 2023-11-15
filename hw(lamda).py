d4dcb26fd2d26ef4e2b40b5ee0120e848b56dda3
shahtich <148983238+shahtich@users.noreply.github.com>
2023-11-15 18:44:40 +0500

    Update hw(lamda).py

--- hw(lamda).py
+++ hw(lamda).py
@@ -8,9 +8,7 @@ print(result)
 S=[13,12,1,22,3232,12,12,144,296,0]
 result=list((filter(lambda x: (x%12==0 or x%13==0),S)))
 print (result)'''
-"from functools import reduce"
+'''from functools import reduce
 A=[12,1,2,2,1,2,3232,0.1]
-'''result=reduce(max,A)
+result=reduce(max,A)
 print(result)'''
-A=sorted(A)
-print(A[0])
